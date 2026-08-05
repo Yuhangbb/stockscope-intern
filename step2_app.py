9a5d4d29747a922fee0c681603855889b9ddd2f0
Yuhangbb <yj474@scarletmail.rutgers.edu>
2026-08-04 20:58:08 -0400

    Add files via upload

--- step2_app.py
+++ step2_app.py
@@ -6,7 +6,6 @@ st.title("📈 My First Stock Page")
 st.write("Hello! This page shows stock prices.")
 
 
-# TODO 2.1
 ticker = st.text_input("Ticker", value="MSFT").upper()
 
 period = st.selectbox("Range", ["5d", "1mo", "3mo", "1y"])
@@ -21,19 +20,3 @@ latest = data["Close"].iloc[-1]
 st.metric("Latest Close", f"${latest:.2f}")
 
 st.line_chart(data["Close"])
-
-# TODO 2.2
-# latest = data["Close"].iloc[-1]
-# st.metric("Latest Close", f"${latest:.2f}")
-
-
-# TODO 2.3
-# ticker = st.text_input("Ticker", value="AAPL").upper()
-
-
-# TODO 2.4
-# st.line_chart(data["Close"])
-
-
-# TODO 2.5 (bonus)
-# period = st.selectbox("Range", ["5d", "1mo", "3mo", "1y"])
